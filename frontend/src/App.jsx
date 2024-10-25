5ea4cf66b8d1f29a65ff2f32e576a3ed3f41a4dc
joshbuzztech <josh@buzztech.ng>
2024-10-25 13:27:07 +0100

    Pages Built

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import MyProfile from "./pages/MyProfile"
 import MyAppointments from "./pages/MyAppointments"
 import Appointment from "./pages/Appointment"
 import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 
 
 const App = () => {
@@ -22,9 +23,10 @@ const App = () => {
           <Route path='/about' element={<About />}/>
           <Route path='/contact' element={<Contact />}/>
           <Route path='/my-profile' element={<MyProfile />}/>
-          <Route path='/my-appointment' element={<MyAppointments />}/>
+          <Route path='/my-appointments' element={<MyAppointments />}/>
           <Route path='/appointment/:docId' element={<Appointment />}/>
         </Routes>
+        <Footer />
     </div>
   )
 }
